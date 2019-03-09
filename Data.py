d822c7e7e31eb3d6d4ffd1f908e9ec41888c3fd4
Dragica Adamovic <d_adamov@cce-fb105-03m.concordia.ca>
2019-03-09 12:47:25 -0500

    Thirs commit

--- Data.py
+++ Data.py
@@ -15,9 +15,10 @@ print(boston_data['feature_names'])
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-
+import numpy as np
 
 boston = pd.DataFrame(boston_data.data, columns=boston_data.feature_names)
+boston.isnull().sum()
 boston.head()
 
 for col in boston:
@@ -28,9 +29,31 @@ for col in boston:
    plt.close()
 
 
-correlation_matrix = boston.corr().roud(2)
+correlation_matrix = boston.corr().round(2)
 heatmap = sns.heatmap(data=correlation_matrix, annot=True)
 myfig = heatmap.get_figure()
 myfig.savefig('output.png')
 plt.close()
+
+Pairplot = sns.pairplot(boston)
+Pairplot.savefig("Pairplot.png")
+plt.close()
+
+
+X = pd.DataFrame(np.c_[boston['LSTAT'], boston['RM']], columns = ['LSTAT','RM'])
+Y = boston_data['target']
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.2, random_state=5)
+print(X_train.shape)
+print(X_test.shape)
+print(Y_train.shape)
+print(Y_test.shape)
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+lin_model = LinearRegression()
+lin_model.fit(X_train, Y_train)
 
